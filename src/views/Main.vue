be86796997173af558a1cb0a26988c84f3b864cf
gabriel.luz <gabriel.luz@hospidata.com.br>
2021-02-05 17:03:51 -0300

    Alterações para novo modelo de visualização

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,33 +1,31 @@
 <template>
   <v-layout>
-    <v-app-bar color="teal lighten-1" dense app>
+    <v-app-bar color="grey lighten-4" dense app>
       <v-app-bar-title>
-        <span class="font-weight-bold">MV</span><span class="font-weight-light">Hospidata - Agendamentos</span>
+        <span class="font-weight-bold" style="color: #1976d2">MV</span
+        ><span class="font-weight-light"
+          ><b>Hospidata</b> - Confirmação de Agendamentos</span
+        >
       </v-app-bar-title>
     </v-app-bar>
 
     <v-container>
       <v-main>
-        <Tabs/>
-
+        <Stepper class="mb-2" />
       </v-main>
     </v-container>
   </v-layout>
 </template>
 
 <script>
-
-
-import Tabs from "@/components/Tabs";
-
+import Stepper from "@/components/Steper";
 export default {
   name: "Main",
-  components:{
-    Tabs
-  }
-}
+  components: {
+    Stepper,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
