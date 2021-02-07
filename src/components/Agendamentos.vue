381f850ac556232cb6a75d6d492b5401a0ea2fa6
gabriel.luz <gabriel.luz@hospidata.com.br>
2021-02-07 13:51:01 -0300

    Novos Arquivos

--- src/components/Agendamentos.vue
+++ src/components/Agendamentos.vue
@@ -1,42 +1,39 @@
 <template>
   <v-row>
-    <v-col v-for="item in agendamentos" :key="item.id" xl="2" lg="4" md="6" sm="12">
-      <v-card elevation="6" color="red lighten-5">
-        <v-toolbar dense flat color="red lighten-2">
-          <span class="font-weight-bold">Item Agendamento:</span> {{ item.nome }}
+    <v-col
+      v-for="item in agendamentos"
+      :key="item.id"
+      xl="2"
+      lg="4"
+      md="6"
+      sm="12"
+    >
+      <v-card elevation="6" class="mb-3">
+        <v-toolbar dark dense flat color="primary" class="ma-0 pa-0">
+          
+          <span class="font-weight-bold">Item Agendamento:</span>
+          {{ item.nome }}
         </v-toolbar>
         <v-card-text>
           <v-row>
-            <v-col lg="12" md="12" sm="12" class="ma-0 pa-1">
-              <v-chip label color="red" dark small>{{ item.status }}</v-chip>
+            <v-col lg="6" md="8" sm="12" class="ma-0 pa-1">
+              <span class="font-weight-bold"> Data: </span
+              >{{ item.dia || "Nao Informada" }}
             </v-col>
             <v-col lg="6" md="8" sm="12" class="ma-0 pa-1">
-              <span class="font-weight-bold"> Data: </span>{{ item.dia || 'Nao Informada' }}
-            </v-col>
-            <v-col lg="6" md="8" sm="12" class="ma-0 pa-1">
-              <span class="font-weight-bold"> Hora: </span>{{ item.hora || 'Nao Informada' }}
+              <span class="font-weight-bold"> Hora: </span
+              >{{ item.hora || "Nao Informada" }}
             </v-col>
             <v-col lg="12" md="12" sm="12" class="ma-0 pa-1">
-              <span class="font-weight-bold"> Medico: </span>{{ item.medico || 'Nao Informado' }}
+              <span class="font-weight-bold"> Medico: </span
+              >{{ item.medico || "Nao Informado" }}
             </v-col>
             <v-col lg="12" md="12" sm="12" class="ma-0 pa-1">
-              <span class="font-weight-bold"> Endereco: </span>{{ item.endereco || 'Nao Informado' }}
+              <span class="font-weight-bold"> Endereco: </span
+              >{{ item.endereco || "Nao Informado" }}
             </v-col>
           </v-row>
         </v-card-text>
-        <v-card-actions class="justify-center">
-          <v-btn-toggle rounded dense>
-            <v-btn  color="teal lighten-1" elevation="3" >
-              Confirmar
-            </v-btn >
-            <v-btn  color="teal lighten-1" elevation="3">
-              Transferir
-            </v-btn>
-            <v-btn  color="teal lighten-1" elevation="3">
-              Cancelar
-            </v-btn>
-          </v-btn-toggle>
-        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
@@ -45,15 +42,12 @@
 <script>
 export default {
   name: "Agendamentos",
-  props:['agendamentos'],
+  props: ["agendamentos"],
   data() {
-    return {
-
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/components/Steper.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step step="1" :complete="e1 > 1">
+        Selecione o Agendamento
+      </v-stepper-step>
+      <v-divider />
+      <v-stepper-step step="2" :complete="e1 > 2">
+        Dados do Agendamento
+      </v-stepper-step>
+      <v-divider />
+      <v-stepper-step step="3" :complete="e1 > 3">
+        Dados Pessoais
+      </v-stepper-step>
+      <v-divider />
+      <v-stepper-step step="4" :complete="e1 > 4"> Conclusão </v-stepper-step>
+    </v-stepper-header>
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <Agendamentos :agendamentos="agendamentosPendentes" />
+        <v-btn color="primary" @click="e1 = 2"> Próximo </v-btn>
+        <v-btn text> Cancel </v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="2">
+        teste 2
+        <v-btn color="primary" @click="e1 = 3"> Próximo </v-btn>
+
+        <v-btn text @click="e1 = 1"> Retornar </v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="3">
+        teste 3
+        <v-btn color="primary" @click="e1 = 4"> Próximo </v-btn>
+        <v-btn text @click="e1 = 2"> Retornar </v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="4"> teste 4 
+
+        <v-btn color="primary" @click="alert('Concluido')"> Concluir </v-btn>
+        <v-btn text @click="e1 = 3"> Retornar </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+</template>
+
+<script>
+import Agendamentos from "@/components/Agendamentos";
+export default {
+  name: "Tabs",
+  components: { Agendamentos },
+  data() {
+    return {
+      e1: 1,
+      agendamentosPendentes: [
+        {
+          id: 1,
+          nome: "Teste de HIV",
+          dia: "28/01/12",
+          hora: "13:00",
+          medico: "Medico Teste",
+          endereco: "Rua Teste, 123",
+          status: "Pagamento nao realizado",
+        },
+        {
+          id: 2,
+          nome: "Teste de COVID",
+          dia: "28/01/12",
+          hora: "13:00",
+          medico: "Medico Teste",
+          endereco: "Rua Teste, 123",
+          status: "Pagamento nao realizado",
+        },
+        {
+          id: 1,
+          nome: "Teste de HIV",
+          dia: "28/01/12",
+          hora: "13:00",
+          medico: "Medico Teste",
+          endereco: "Rua Teste, 123",
+          status: "Pagamento nao realizado",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
