ef343d1608eaa507ce6f8a0b132004345bd16317
gabriel.luz <gabriel.luz@hospidata.com.br>
2021-02-22 18:01:25 -0300

    Cancelamento

--- app/Agendamentos/agendamentos.js
+++ app/Agendamentos/agendamentos.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const db = require('../dbConnect')
 
 router.get('/consultaAgendamentos', (req, res) => {
-    (async() => {
+    (async () => {
         const client = await db.pool.connect()
         try {
             const agendamentos = await client.query(
-                    `SELECT
+                `SELECT
                     agend.id_agendamento AS "idAgendamento",
                     p.id_paciente AS "idPaciente",
                     TO_CHAR(agend.data_atend_iten_agend, 'DD/MM/YYYY') AS "dtAgendamento",
@@ -46,6 +46,178 @@ router.get('/consultaAgendamentos', (req, res) => {
         res.status(400).json(e)
     })
 
+})
+
+router.delete('/cancelar/:idHorario/:motivoCancelamento', (req, res) => {
+    (async () => {
+
+        const client = await db.pool.connect()
+
+        try {
+
+            let converteDataPostgres = (date) => {
+                const a = new Date(date)
+                const dia = a.getUTCDate()
+                const mes = a.getUTCMonth() + 1
+                const ano = a.getFullYear()
+                return `${ano}-${mes}-${dia}`
+            }
+
+
+            let agendamentoOriginal = await client.query(`select * from sigh.agendamentos where id_agendamento = ${req.params.idHorario}`)
+            agendamentoOriginal = agendamentoOriginal.rows
+
+            await client.query(`update
+                                       sigh.agendamentos
+                                    set
+                                       nm_paciente = null
+                                       , cod_paciente = null
+                                       , cod_convenio = null
+                                       , cod_categoria = null
+                                       , cod_procedimento =  (select cod_procedimento from sigh.agendas where id_agenda = ${agendamentoOriginal[0].cod_agenda})
+                                       , hora_atendimento = null
+                                       , cod_especialidade = null
+                                       , cod_procedencia = null
+                                       , cod_encaminhamento = null
+                                       , data_marcacao = null
+                                       , cod_setor = (select cod_setor from sigh.agendas where id_agenda = ${agendamentoOriginal[0].cod_agenda})
+                                       , cod_sala = (select cod_sala from sigh.agendas where id_agenda = ${agendamentoOriginal[0].cod_agenda})
+                                       , cod_cbo = null
+                                       , compl_iten_agendamento = null
+                                       , consulta_retorno = 'N'
+                                       , meio_agendamento = null
+                                       , cobrar = 'N'
+                                       , fone_paciente = null
+                                       , nm_responsavel = null
+                                       , cod_grau_parentesco = null
+                                       , fatura_iten_agendado = null
+                                       , cod_grupo_sus = null
+                                       , cod_tipo_sus = null
+                                       , cod_especialidade_sus = null
+                                       , cod_subespecialidade = null
+                                       , em_edicao = false
+                                       , cod_fia = null
+                                       , sequencial = null
+                                       , duracao_consulta = (select intervalo_consulta from sigh.agendas where id_agenda = ${agendamentoOriginal[0].cod_agenda})
+                                       , tratamento = false
+                                       , cod_agendamento_tratamento = null
+                                       , celular_paciente = null
+                                       , falta = null
+                                       , tipo_atend_tiss = 4
+                                       , tipo_consulta_tiss = 1
+                                       , email = null
+                                       , autorizado_convenio = false
+                                       , data_origem_marcacao = null
+                                       , data_transferencia = null
+                                       , data_nasc = null
+                                       , nm_medico_solicitante = null
+                                       , cod_prescricao = null
+                                       , cod_agendamento_integracao_ag = null
+                                   where
+                                        id_agendamento = ${agendamentoOriginal[0].id_agendamento}`)
+
+
+            //Gera o novo registro na agenda com o motivo agendamento
+            const novoRegistro = await client.query(`
+                                            insert into
+                                            sigh.agendamentos_desistencias (
+                                                 cod_agenda
+                                                , data_atend_iten_agend
+                                                , hora_atend_iten_agen
+                                                , nm_paciente
+                                                , cod_paciente
+                                                , cod_convenio
+                                                , cod_categoria
+                                                , cod_procedimento
+                                                , hora_atendimento
+                                                , duracao_consulta
+                                                , cod_especialidade
+                                                , cod_procedencia
+                                                , cod_encaminhamento
+                                                , data_marcacao
+                                                , cod_setor
+                                                , cod_sala
+                                                , cod_cbo
+                                                , compl_iten_agendamento
+                                                , consulta_retorno
+                                                , meio_agendamento
+                                                , cobrar
+                                                , fone_paciente
+                                                , nm_responsavel
+                                                , cod_grau_parentesco
+                                                , fatura_iten_agendado
+                                                , cod_grupo_sus
+                                                , cod_tipo_sus
+                                                , cod_especialidade_sus
+                                                , cod_subespecialidade
+                                                , em_edicao
+                                                , cod_fia
+                                                , tratamento
+                                                , cod_agendamento_tratamento
+                                                , celular_paciente
+                                                , email
+                                                , autorizado_convenio
+                                                , data_nasc
+                                                , nm_medico_solicitante
+                                                , cod_prescricao
+                                                , cod_agendamento_integracao_ag
+                                                , cod_motivo_cancelamento)
+                                            values(
+                                               ${agendamentoOriginal[0].cod_agenda}
+                                            , '${converteDataPostgres(agendamentoOriginal[0].data_atend_iten_agend)}'
+                                            , '${agendamentoOriginal[0].hora_atend_iten_agen}'
+                                            , '${agendamentoOriginal[0].nm_paciente}'
+                                            , ${agendamentoOriginal[0].cod_paciente}
+                                            , ${agendamentoOriginal[0].cod_convenio}
+                                            , ${agendamentoOriginal[0].cod_categoria}
+                                            , ${agendamentoOriginal[0].cod_procedimento}
+                                            , ${agendamentoOriginal[0].hora_atendimento}
+                                            , '${agendamentoOriginal[0].duracao_consulta}'
+                                            , ${agendamentoOriginal[0].cod_especialidade}
+                                            , ${agendamentoOriginal[0].cod_procedencia}
+                                            , ${agendamentoOriginal[0].cod_encaminhamento}
+                                            , '${converteDataPostgres(agendamentoOriginal[0].data_marcacao)}'
+                                            , ${agendamentoOriginal[0].cod_setor}
+                                            , ${agendamentoOriginal[0].cod_sala}
+                                            , ${agendamentoOriginal[0].cod_cbo}
+                                            , ' ' 
+                                            , '${agendamentoOriginal[0].consulta_retorno}'
+                                            , ${agendamentoOriginal[0].meio_agendamento}
+                                            , '${agendamentoOriginal[0].cobrar}'
+                                            , ${agendamentoOriginal[0].fone_paciente}
+                                            , ${agendamentoOriginal[0].nm_responsavel}
+                                            , ${agendamentoOriginal[0].cod_grau_parentesco}
+                                            , ${agendamentoOriginal[0].fatura_iten_agendado}
+                                            , ${agendamentoOriginal[0].cod_grupo_sus}
+                                            , ${agendamentoOriginal[0].cod_tipo_sus}
+                                            , ${agendamentoOriginal[0].cod_especialidade_sus}
+                                            , ${agendamentoOriginal[0].cod_subespecialidade}
+                                            , ${agendamentoOriginal[0].em_edicao}
+                                            , ${agendamentoOriginal[0].cod_fia}
+                                            , ${agendamentoOriginal[0].tratamento}
+                                            , ${agendamentoOriginal[0].cod_agendamento_tratamento}
+                                            , '${agendamentoOriginal[0].celular_paciente}'
+                                            , '${agendamentoOriginal[0].email}'
+                                            , ${agendamentoOriginal[0].autorizado_convenio}
+                                            , '${converteDataPostgres(agendamentoOriginal[0].data_nasc)}'
+                                            , ${agendamentoOriginal[0].nm_medico_solicitante}
+                                            , ${agendamentoOriginal[0].cod_prescricao}
+                                            , ${agendamentoOriginal[0].cod_agendamento_integracao_ag}
+                                            , ${req.params.motivoCancelamento})`)
+                .then(() => {
+                    res.status(200).json("OK")
+                })
+                .catch(() => {
+                    res.status(407).json("Erro de Negócio")
+                })
+
+        } finally {
+            client.release()
+        }
+    })().catch((e) => {
+        console.log(e)
+        res.status(417).json('Erro de negocio' + e)
+    })
 })
 
 module.exports = router
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,41 +1,41 @@
 <template>
   <v-layout>
     <v-app-bar color="grey lighten-4" dense app>
+      
       <v-app-bar-title>
-        <span class="font-weight-bold" style="color: #1976d2">MV</span
-        ><span class="font-weight-light"
-          ><b>Hospidata</b> - Confirmação de Agendamentos</span
-        >
+      
+        <span class="font-weight-bold" style="color: #1976d2">MV</span><span class="font-weight-light"><b>Hospidata</b>
+          - Confirmação de Agendamentos</span>
+      
       </v-app-bar-title>
+      
       <v-spacer></v-spacer>
+      
       <v-btn plain to="posts" active-class="active-class">Posts</v-btn>
-      <v-btn plain to="institucional" active-class="active-class"
-        >Institucional</v-btn
-      >
+      
+      <v-btn plain to="institucional" active-class="active-class">Institucional</v-btn>
+      
       <v-badge bordered color="error" overlap :content="countAgendamentos" class="mr-4 mt-0">
-        <v-btn plain to="agendamentos" active-class="active-class"
-          >Agendamentos</v-btn
-        >
+        <v-btn plain to="agendamentos" active-class="active-class">Agendamentos</v-btn>
       </v-badge>
+      
       <v-menu offset-y>
+        
         <template v-slot:activator="{ on, attrs }">
           <v-btn icon color="primary" v-bind="attrs" v-on="on">
             <v-icon>mdi-account-circle</v-icon>
           </v-btn>
-          <v-divider vertical/>
+          <v-divider vertical />
         </template>
+
         <v-list>
-          <v-list-item
-            :to="item.to"
-            v-for="(item, index) in items"
-            :key="index"
-          >
-            <v-list-item-title v-e>{{ item.title }}</v-list-item-title>
+          <v-list-item :to="item.to" v-for="(item, index) in items" :key="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
     </v-app-bar>
-
+  
     <v-container>
       <v-main>
         <router-view></router-view>
@@ -45,29 +45,39 @@
 </template>
 
 <script>
-export default {
-  name: "Main",
-  data() {
-    return {
-      items: [
-        { title: "Perfil", to: "/perfil" },
-        { title: "Historicos", to: "/historico" },
-        { title: "Pagamentos", to: "/pagamentos" },
-        { title: "Sair" },
-      ],
-    };
-  },
-  computed:{
-    countAgendamentos(){
-      return this.$store.state.paciente.agendamentos.length
-    }
-  }
-};
+  export default {
+    name: "Main",
+    data() {
+      return {
+        items: [{
+            title: "Perfil",
+            to: "/perfil",
+          },
+          {
+            title: "Historicos",
+            to: "/historico",
+          },
+          {
+            title: "Pagamentos",
+            to: "/pagamentos",
+          },
+          {
+            title: "Sair",
+          },
+        ],
+      };
+    },
+    computed: {
+      countAgendamentos() {
+        return this.$store.state.paciente.agendamentos.length;
+      },
+    },
+  };
 </script>
 
 <style scoped>
-.active-class {
-  text-decoration: underline 3px;
-  text-decoration-color: #1976d2;
-}
+  .active-class {
+    text-decoration: underline 3px;
+    text-decoration-color: #1976d2;
+  }
 </style>
